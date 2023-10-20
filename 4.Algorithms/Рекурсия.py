7ebd837aae64782798be2be02c8924ce26f956ff
Valeogamer <zhilin.valentin.72@gamil.com>
2023-10-20 22:16:35 +0300

    fix: refactor dir

--- 4.Algorithms/Рекурсия.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 0:  # базовый случай
-        return 1
-    else:  # рекурсивный случай
-        return n * factorial(n - 1)
-
-
-if __name__ == '__main__':
-    print(factorial(995))  # максимальная глубина рекурсии
